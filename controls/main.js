3d13bae195a44a168bd5a1e80f7d20c94fe57daa
Micael Rocha <69124656+eu-micaeu@users.noreply.github.com>
2024-07-06 19:26:10 -0300

    Co-authored-by: BrenaFreitas <BrenaFreitas@users.noreply.github.com>

--- controls/main.js
+++ controls/main.js
@@ -3,17 +3,28 @@ const express = require('express');
 const router = express.Router();
 
 const data = {
+
     imagePath: 'img/Logo.png',
     styleIndexPath: 'css/index.css',
-    image1 : 'img/image1.png',
-    image2 : 'img/image2.png',
-    image3 : 'img/image3.png',
-    image4 : 'img/image4.png',
-    image5 : 'img/image5.png'
+
 };
 
+const images = [
+
+    'img/image1.png',
+    'img/image2.png',
+    'img/image3.png',
+    'img/image4.png',
+    'img/image5.png'
+    
+];
+
 router.get("/", (req, res) => {
-    res.render("index", data);
+
+    const combinedData = Object.assign({}, data, {images: images});
+
+    res.render("index", combinedData);
+
 });
 
 module.exports = router;

93ab55b63e1b4536b5bf9f6778a64b56ca9a951e
Micael Rocha <69124656+eu-micaeu@users.noreply.github.com>
2024-07-07 18:08:59 -0300

    Refactor CSS files for consistent styling and improved responsiveness

--- public/images/itemRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const itemController = require('../controllers/itemController');
-const router = express.Router();
-
-router.post('/registerItem', itemController.registerItem);
-router.get('/pegarItens', itemController.pegarItens);
-
-module.exports = router;
